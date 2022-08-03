1413cab3d662083c52bdd8075f3857bfce836ef3
cassiano-dio <cassiano@dio.me>
2022-08-03 14:02:04 -0300

    Update

--- src/main/java/com/api/example/app/Application.java
+++ src/main/java/com/api/example/app/Application.java
@@ -2,8 +2,10 @@ package com.api.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.api.example"})
 public class Application {
 
     public static void main(String[] args) {
